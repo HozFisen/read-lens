47410f60310e2a0cbc6fb3dc16b76670f8bd291a
HozFisen <hozfisen@gmail.com>
2025-11-14 10:00:47 +0700

    feat: find all user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,10 +55,18 @@ class UserController {
       if (!user) return res.status(404).json({ message: 'User not found' });
 
       res.json(user);
-    } catch (err) {
-      res.status(500).json({ message: 'Server error' });
+    } catch (error) {
+      next(error)
     }
   };
+  static async allUser(req, res, next) {
+    try {
+      const users = await User.findAll()
+      res.status(200).json(users)
+    } catch (error) {
+      next(error)
+    }
+  }
 
   static async updateUsername(req, res, next) {
     try {
@@ -88,8 +96,8 @@ class UserController {
       if (!removed) return res.status(404).json({ message: 'User not found' });
 
       res.json({ message: 'User deleted' });
-    } catch (err) {
-      res.status(500).json({ message: 'Server error' });
+    } catch (error) {
+      next(error)
     }
   }
 
